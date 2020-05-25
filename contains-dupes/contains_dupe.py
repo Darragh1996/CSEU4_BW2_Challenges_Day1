2376f7f97128a294b35a4b0807baa2512344a153
Darragh Ferry <darraghferry@hotmail.com>
2020-05-25 13:44:39 +0100

    contains_dupe

--- contains-dupes/contains_dupe.py
+++ contains-dupes/contains_dupe.py
@@ -1,2 +1,15 @@
 def containsDuplicate(num):
-    pass
+    original_length = len(num)
+    new_length = len(set(num))
+
+    if original_length == new_length:
+        return False
+    return True
+
+
+# tests
+test1 = containsDuplicate([1, 2, 3, 1])
+test2 = containsDuplicate([1, 2, 3, 4])
+test3 = containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
+
+print(test1, test2, test3)
